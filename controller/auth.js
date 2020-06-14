92ff705c72d7c3ede7aa1c2f4f70921a511a3ed8
David Akpughe <davak@Davids-MacBook-Air.local>
2020-06-14 23:03:11 +0100

    change check to phonemunber

--- controller/auth.js
+++ controller/auth.js
@@ -18,7 +18,7 @@ exports.register = async (req, res, next) => {
   const { phonenumber } = req.query;
 
   try {
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ phonenumber });
 
     if (user) {
       return res
@@ -79,6 +79,58 @@ exports.getUserById = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { phonenumber, password } = req.body;
+
+  try {
+    //See if user exits
+    let user = await User.findOne({ phonenumber });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Phone number does not exist' }] });
+    }
+
+    //matching user details
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+    }
+
+    //Return JWT
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(payload, configg.get('jwtSecret'), (err, token) => {
+      if (err) throw err;
+      res.json({
+        token,
+        user: {
+          id: user._id,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+          phonenumber: user.phonenumber,
+          staus: user.status,
+        },
+      });
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const authController = require('../controller/auth');
 const auth = require('../middleware/auth');
 
 router.post('/', authController.register);
+router.post('/login', authController.login);
 router.get('/', auth, authController.getUserById);
+
 
 module.exports = router;
